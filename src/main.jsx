1a238e0b5427fe0c3a08b9a0b5fdcd02339ac30a
jaornelas <ornelalf001@gmail.com>
2025-02-18 16:19:06 -0600

    create main.jsx file using components visualized

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//use of bootstrap to customize page
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import Error from './Error.jsx';
+import AboutMe from './AboutMe.jsx';
+import Portfolio from './Portfolio.jsx';
+import Contact from './Contact.jsx';
+import Resume from './Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true, 
+        element: <AboutMe />
+      },
+      {
+        path: 'portfolio', 
+        element: <Portfolio />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
+      {
+        path: 'resume',
+        element: <Resume />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router} />
+);
