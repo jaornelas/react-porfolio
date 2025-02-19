42c2928087174ea9ab6dbd9ccf02f2e4e244f767
jaornelas <ornelalf001@gmail.com>
2025-02-18 23:38:59 -0600

    page component and trying to debug crash

--- src/components/Navigate.jsx
+++ src/components/Navigate.jsx
@@ -1,45 +1,61 @@
 // TODO: Use the react-router-dom to create links for About Me, Portfolio, Contact, and Resume.
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
-function Navigate() {
-    const currentPage = useLocation().pathname;
+function Nav() {
+  const currentPage = useLocation().pathname;
 
-    return (
-        <ul className='nav justify-content-end mb-2'>
-            <li className='nav-item'>
-                <Link
-                    to='/'
-                    className={currentPage === '/' ? 'nav-link link-success fs-2' : 'nav-link link-light fs-2'}
-                >
-                    About Me
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link
-                    to='/portfolio'
-                    className={currentPage === '/portfolio' ? 'nav-link link-success fs-2' : 'nav-link link-light fs-2'}
-                >
-                    Portfolio
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link
-                    to='/contact'
-                    className={currentPage === '/contact' ? 'nav-link link-success fs-2' : 'nav-link link-light fs-2'}
-                >
-                    Contact
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link
-                    to='/resume'
-                    className={currentPage === '/resume' ? 'nav-link link-success fs-2' : 'nav-link link-light fs-2'}
-                >
-                    Resume
-                </Link>
-            </li>
-        </ul>
-    );
+  return (
+    <ul className="nav justify-content-end mb-2">
+      <li className="nav-item">
+        <Link
+          to="/"
+          className={
+            currentPage === "/"
+              ? "nav-link link-success fs-2"
+              : "nav-link link-light fs-2"
+          }
+        >
+          About Me
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/portfolio"
+          className={
+            currentPage === "/portfolio"
+              ? "nav-link link-success fs-2"
+              : "nav-link link-light fs-2"
+          }
+        >
+          Portfolio
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/contact"
+          className={
+            currentPage === "/contact"
+              ? "nav-link link-success fs-2"
+              : "nav-link link-light fs-2"
+          }
+        >
+          Contact
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/resume"
+          className={
+            currentPage === "/resume"
+              ? "nav-link link-success fs-2"
+              : "nav-link link-light fs-2"
+          }
+        >
+          Resume
+        </Link>
+      </li>
+    </ul>
+  );
 }
 
-export default Navigate;
+export default Nav;
