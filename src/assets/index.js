9725107f166555e1f398d8f52f70bbcb2f407b85
jaornelas <ornelalf001@gmail.com>
2025-02-23 21:34:18 -0600

    add links and images to portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as payrollTracker } from '../img/payrollCalc.jpg';
+export { default as preworkStudyGuide } from '../img/led-wall.webp';
+//export { default as pastelPuzzels } from './projects/pastelPuzzels.jpg';
+//export { default as runBuddy } from './projects/runBuddy.jpg';
+//export { default as surfReport } from './projects/surfReport.jpg';
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,26 +1,25 @@
 import { useState } from 'react';
 import Project from '../components/Project.jsx';
-import prework from '../img/led-wall.webp'
 
 // TODO: Implement the Project components in two columns side-by-side. Use props to make each Project different.
 function Portfolio() {
   const [projects] = useState([
     {
-      name: 'Prework Study Guide',
+      name: 'preworkStudyGuide',
       repo: 'https://github.com/jaornelas/prework-study-guide',
       link: 'https://jaornelas.github.io/prework-study-guide/',
       description: ''
     },
     {
-      name: 'Prework Study Guide',
-      repo: 'https://github.com/jaornelas/prework-study-guide',
-      link: 'https://jaornelas.github.io/prework-study-guide/',
+      name: 'payrollTracker',
+      repo: 'https://github.com/jaornelas/SQLemployee-tracker',
+      link: 'https://drive.google.com/file/d/1NbH16kYne8e344qzuBRC71fpoX1Y298n/view',
       description: ''
     },
     {
-      name: 'Prework Study Guide',
-      repo: 'https://github.com/jaornelas/prework-study-guide',
-      link: 'https://jaornelas.github.io/prework-study-guide/',
+      name: 'Build-A-Car',
+      repo: 'https://github.com/jaornelas/typescript-vehiclebuilder',
+      link: 'https://drive.google.com/file/d/1vJpEogk0VnR-fd6eGw4FLYroBvAg6ZfI/view',
       description: ''
     }
   ])
