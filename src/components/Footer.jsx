21e5de6001c48097989b15f1d6a83929e77e7855
jaornelas <ornelalf001@gmail.com>
2025-02-23 21:52:47 -0600

    start the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,22 @@
-function Footer () {
+import './Footer.css';
+import GitHub from './GitHub.jsx';
+import LinkedIn from '../images/In-White-72.png';
+import X from '../images/x-logo-black.png';
 
+function Footer() {
+const link = 'https://github.com/jaornelas';
+
+    return (
+        <div>
+            <div className="container text-center">
+                <GitHub link={link} />
+                <a href='https://www.linkedin.com/in/colton-doling-893bb0341/' target='_blank'><img src={LinkedIn} className='icon p-5'/></a>
+                <a href='https://x.com/colton_doling' target='_blank'><img src={X} className='icon rounded ms-5 p-2' style={{backgroundColor: '#ffffff', width: 50}}/></a>
+            </div>
+            <div className='text-center ms-3'>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
