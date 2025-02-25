742a8d221da3d3675ab1b4c89f0d66a08176bb7b
jaornelas <ornelalf001@gmail.com>
2025-02-24 21:46:29 -0600

    format aboutMe

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,28 +2,24 @@ import Profile from '../img/profilepic.jpeg';
 
 function About() {
     return (
-        <section>
-            <div>
-                <div>
-                    <h2>
-                        About Me
-                    </h2>
-                    <br />
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <img src={Profile} className="rounded-circle" style={{ width: 200, marginRight: '20px' }} alt="Profile Picture" />
-                        <br />
-                        <br />
-                        <p>
-                            Hello! I'm Jose Alfredo Ornelas and I am a full stack developer from Wisconsin. Over the past few months, I've been enhancing my developer skills by learning various libararies and frameworks available to all developers
-                            (TypeScript, Express, Node, React, etc). In my sparetime I enjoy reading books, going camping, and watching birds.
-                            <br/> <br/>
-                            My goals as I transition into a software engineering role are to continue adding more langugages to my repertoire and collaborate with other developers in my area to create meaningful projects.
-                            Please feel free to browse my site and visit the different repositories created during my Web Developer Bootcamp.
-                        </p>
-                    </div>
-                </div>
+        <div>
+            <h2>
+                About Me
+            </h2>
+            <br />
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={Profile} className="rounded-circle" style={{ width: 200, marginRight: '20px' }} alt="Profile Picture" />
+                <br />
+                <br />
+                <p>
+                    Hello! I'm Jose Alfredo Ornelas and I am a full stack developer from Wisconsin. Over the past few months, I've been enhancing my developer skills by learning various libararies and frameworks available to all developers
+                    (TypeScript, Express, Node, React, etc). In my sparetime I enjoy reading books, going camping, and watching birds.
+                    <br /> <br />
+                    My goals as I transition into a software engineering role are to continue adding more langugages to my repertoire and collaborate with other developers in my area to create meaningful projects.
+                    Please feel free to browse my site and visit the different repositories created during my Web Developer Bootcamp.
+                </p>
             </div>
-        </section>
+        </div>
     );
 }
 
