313e923a3116031e457459a9838fae08b59fd13f
jaornelas <ornelalf001@gmail.com>
2025-02-24 21:43:38 -0600

    add comments to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+//Import necessary modules and components
 import { useState } from 'react'
 import Header from './components/Header';
 import Navigate from './components/Navigate'
 import Footer from './components/Footer';
 import Page from './components/Page';
 import { useLocation } from 'react-router-dom';
-// import './App.css'
 
 function App() {
+  //Get the current page path using useLocation hook
   const currentPage = useLocation().pathname;
 
+  //Define a style object for the packground color
   const style = {
     background: '#000000'
   }
