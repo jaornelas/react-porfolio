115427d33d5b39dc5ba9af08e021305fc197aa37
jaornelas <ornelalf001@gmail.com>
2025-02-25 21:07:29 -0600

    change footer bgcolor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,6 @@ function Footer() {
                 <a href='https://www.linkedin.com/in/jos%C3%A9-alfredo-ornelas-4bb38a166/' target='_blank'><img src={LinkedIn} className='icon p-5'/></a>
                 <a href='https://x.com/fredsejo?s=21' target='_blank'><img src={X} className='icon rounded ms-5 p-2' style={{backgroundColor: '#ffffff', width: 50}}/></a>
             </div>
-            <div className='text-center ms-3'>
-            </div>
         </div>
     );
 }
