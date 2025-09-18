1ec8b3a39968ebec86745a0c8ad5e70c05450b5c
jaornelas <ornelalf001@gmail.com>
2025-09-18 18:26:36 -0500

    add on index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,3 +2,4 @@ export { default as employeeTracker } from '../img/employeeDB.jpg';
 export { default as preworkStudyGuide } from '../img/led-wall.webp';
 export { default as carBuilder } from '../img/carBuilder.jpg';
 export { default as budgetCalculator } from '../img/payrollCalc.jpg';
+export { default as spaceBoats } from '../img/spaceBoats.png'
